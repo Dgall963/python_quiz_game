886b25842c7159d650d2ae3e1d64f53d6fb21fa7
Dgall963 <galligans@twc.com>
2021-03-07 12:59:13 -0500

    python_quiz_game commit

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-from question_model import Question
-from data import question_data
-from quiz_brain import QuizBrain
-
-print ('This is a quiz game that tests your knowledge of Technology.')
-
-question_bank = []
-for question in question_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
-    new_question = Question(q_text=question_text, q_answer=question_answer)
-    question_bank.append(new_question)
-        
-quiz = QuizBrain(question_bank)
-
-while quiz.still_has_questions():
-    quiz.next_question()
-
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
-
 def main():
-    
-    restart = input("Would you like to restart this program?")
-    if restart == "yes" or restart == "y":
-        print("Okay, I'll restart the quiz for you.")
-        main()
-    if restart == "n" or restart == "no":
-        print ("Program terminating. Goodbye.")
+
+    from question_model import Question
+    from data import question_data
+    from quiz_brain import QuizBrain
+
+    print ('Welcome! This is a quiz game that tests your knowledge of Computer Technology.')
+
+    question_bank = []
+    for question in question_data:
+        question_text = question["question"]
+        question_answer = question["correct_answer"]
+        new_question = Question(q_text=question_text, q_answer=question_answer)
+        question_bank.append(new_question)
+            
+    quiz = QuizBrain(question_bank)
+
+    while quiz.still_has_questions():
+        quiz.next_question()
+
+    print("You've completed the quiz")
+    print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
+    restart=input("Would you like to take the quiz again").lower()
+    if restart == "yes":
+       main()
+    else:     
+        exit()
+
 main()
-    
